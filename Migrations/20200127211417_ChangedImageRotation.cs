7a2d20b50c9db3f87eeec69438695c0411f8d94d
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2020-01-27 16:32:42 -0500

    added image rotation

---
+++ Migrations/20200127211417_ChangedImageRotation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIPinellasAleTrail.Migrations
+{
+    public partial class ChangedImageRotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
